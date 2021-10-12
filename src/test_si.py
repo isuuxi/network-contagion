620a836ed0be1ea160951f4eacc47fa0315d9ed5
Isa <juno_ivf@web.de>
2021-10-12 18:23:32 +0200

    csv output writer

--- src/test_si.py
+++ src/test_si.py
@@ -2,6 +2,8 @@ import SI_model
 import numpy as np
 import networkx as nx
 import utils
+import time 
+import csv
 
 def run_model(t, n, p):
     '''
@@ -15,15 +17,32 @@ def run_model(t, n, p):
             time_series: time series of the number of infected nodes in all time steps
             history: time step of infection for each node
     '''
+    start_time = time.time()
     G = SI_model.create_network(n, p)
     A, infected, infprob_indiv_nodes = SI_model.init_network(G)
-    time = np.arange(t)
     time_series = np.zeros(t) 
     process = utils.ContagionProcess(A, infprob_indiv_nodes, "SI_cont", infected)
     for t in range(t):
         process.step()
         time_series[t] = np.sum(process.infected)
-    return time_series, process.history
+    runtime = (time.time() - start_time)
+    print("--- %s temporal inner seconds ---" % runtime)
+    return time_series, process.history, runtime
 
-time_series, history = run_model(20, 500, 0.01)
-print (f"Infections are {time_series} and the history is {history}")
+parameters = [20, 1000, 0.004]
+print(parameters)
+time_series, history, runtime = run_model(parameters[0], parameters[1], parameters[2])
+print (f"Infections are {time_series} ")
+#and the history is {history}")
+
+header = ['Parameters', 'Time_series', 'Node_history', 'runtime in seconds']
+data = [parameters, time_series, history, runtime]
+
+with open('results.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write the data
+    writer.writerow(data)
